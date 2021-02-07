a5b9be24dd82e8ee7e5d404d6f4c6f33e737166f
celiaxiao <c6xia@ucsd.edu>
2021-02-07 15:13:12 +0800

    Create pdfParser.py

---
+++ pdfParser.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import sys
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+from io import StringIO
+
+'''
+* This file take into one argument as input: filename.
+* And it would save the content into a python variable, and print the
+* String value of the pdf content.
+* usage: pdfParser.py <filename>
+'''
+
+output_string = StringIO()
+with open(sys.argv[1], 'rb') as in_file:
+    parser = PDFParser(in_file)
+    doc = PDFDocument(parser)
+    rsrcmgr = PDFResourceManager()
+    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+
+print(output_string.getvalue())
+#return output_string.getvalue()
